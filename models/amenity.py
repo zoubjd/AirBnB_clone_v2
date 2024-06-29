a6a00e8e034136ffe090a26ea9ca78c970c3843f
oneameeeena <aminaouaqqaouraho@gmail.com>
2024-06-29 07:12:55 -0700

    hbnb

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel
+# models/amenity.py
+
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from models.base_model import BaseModel, Base
+from models.place import place_amenity
+
+class Amenity(BaseModel, Base):
+    __tablename__ = 'amenities'
+    
+    name = Column(String(128), nullable=False)
+    place_amenities = relationship('Place', secondary=place_amenity, viewonly=False)
 
 
-class Amenity(BaseModel):
-    name = ""
--- models/place.py
+++ models/place.py
@@ -1,22 +1,31 @@
 #!/usr/bin/python3
-""" Place Module for HBNB project """
+# models/place.py
+
+from sqlalchemy import Column, String, ForeignKey, Table
+from sqlalchemy.orm import relationship
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, Integer, Float
-from sqlalchemy.orm import relationship, ForeignKey
 
+# Define the place_amenity table for the many-to-many relationship
+place_amenity = Table('place_amenity', Base.metadata,
+                      Column('place_id', String(60), ForeignKey('places.id'), primary_key=True, nullable=False),
+                      Column('amenity_id', String(60), ForeignKey('amenities.id'), primary_key=True, nullable=False))
 
-class Place(BaseModel):
-    """ A place to stay """
+class Place(BaseModel, Base):
     __tablename__ = 'places'
-    city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
-    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
-    name = Column(String(128), nullable=False)
-    description = Column(String(1024), nullable=True)
-    number_rooms = Column(Integer, nullable=False, default=0)
-    number_bathrooms = Column(Integer, nullable=False, default=0)
-    max_guest = Column(Integer, nullable=False, default=0)
-    price_by_night = Column(Integer, nullable=False, default=0)
-    latitude = Column(Float, nullable=True)
-    longitude = Column(Float, nullable=True)
-    amenity_ids = []
-    reviews = relationship("Review", backref="place", cascade="all, delete, delete-orphan")
+    
+    # Existing attributes...
+    
+    amenities = relationship('Amenity', secondary=place_amenity, viewonly=False)
+    
+    # For FileStorage
+    @property
+    def amenities(self):
+        """Getter attribute amenities that returns the list of Amenity instances."""
+        return [amenity for amenity in storage.all(Amenity).values() if amenity.id in self.amenity_ids]
+
+    @amenities.setter
+    def amenities(self, value):
+        """Setter attribute amenities that handles append method for adding an Amenity.id."""
+        if isinstance(value, Amenity):
+            self.amenity_ids.append(value.id)
+
